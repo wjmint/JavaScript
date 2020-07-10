bcf0ab9206d72ebabe40e234b123eb682ebe2ecf
wjmint <jamie060529@gmail.com>
2020-07-10 22:41:09 +0900

    completed lab_3

--- js_hw/lab_3.js
+++ js_hw/lab_3.js
@@ -3,38 +3,56 @@ const readline = require('readline').createInterface({
 	output: process.stdout,
 });
 
-// problem 1
-readline.question('Input a integer\n>', input => {
-	console.log('Output(Number) ->', Number(input));
-	console.log('Output(String) ->', String(input));
+// // problem 1
+// readline.question('Input a integer\n>', input => {
+// 	console.log('Output(Number) ->', Number(input));
+// 	console.log('Output(String) ->', String(input));
+// 	readline.close();
+// });
+
+// // problem 2 - 1 (Number input)
+// let width = 0,
+// 	length = 0,
+// 	area = 0;
+// readline.question('Width: ', width_input => {
+// 	width = width_input;
+// 	readline.question('Length: ', length_input => {
+// 		length = length_input;
+// 		area = width * length;
+// 		console.log('Area ->', area);
+// 		readline.close();
+// 	});
+// });
+
+// // problem 2 - 2 (Float input)
+// let width = 0,
+// 	length = 0,
+// 	area = 0;
+
+// readline.question('Width: ', width_input => {
+// 	width = width_input;
+// 	readline.question('Length: ', length_input => {
+// 		length = length_input;
+// 		area = width * length;
+// 		console.log('Area ->', area.toPrecision(4));
+// 		readline.close();
+// 	});
+// });
+
+// // problem 3
+// readline.question('Input (Float)\n>', float_input => {
+// 	console.log('Round off ->', Math.round(float_input));
+// 	readline.close();
+// });
+
+// // problem 4 - 1 (Lower case)
+// readline.question('Input(lower case)\n>', lower_case => {
+// 	console.log('Output(upper case) ->', lower_case.toUpperCase());
+// 	readline.close();
+// });
+
+// // problem 4 - 2 (Upper case)
+readline.question('Input(upper case)\n>', upper_case => {
+	console.log('Output(lower case) ->', upper_case.toLowerCase());
 	readline.close();
-});
-
-// problem 2 - 1 (Number input)
-let width = 0,
-	length = 0,
-	area = 0;
-readline.question('Width: ', width_input => {
-	width = width_input;
-	readline.question('Length: ', length_input => {
-		length = length_input;
-		area = width * length;
-		console.log('Area ->', area);
-		readline.close();
-	});
-});
-
-// problem 2 - 2 (Float input)
-let width = 0,
-	length = 0,
-	area = 0;
-
-readline.question('Width: ', width_input => {
-	width = width_input;
-	readline.question('Length: ', length_input => {
-		length = length_input;
-		area = width * length;
-		console.log('Area ->', area.toPrecision(4));
-		readline.close();
-	});
 });
